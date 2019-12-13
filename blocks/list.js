4295fc17db2f08e134f909d1e717afb32c777a6b
Francis Bulus <notchera@gmail.com>
2019-12-13 11:37:33 +0100

    fleshed out header, list, and paragraph blocks

---
+++ blocks/list.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactHtmlParser from "react-html-parser";
+
+const List = data => {
+  if (!data) return "";
+
+  let content = [],
+    type = "unordered";
+
+  if (typeof data === "string") content.push(data);
+  else if (typeof data === "object") {
+    if (data.items && Array.isArray(data.items))
+      content = data.items.map((item, index) => (
+        <li key={index} className="cdx-list__item">
+          {ReactHtmlParser(item)}
+        </li>
+      ));
+    if (data.style && (data.style === "ordered" || "unordered"))
+      type = data.style;
+  }
+
+  if (content.length <= 0) return "";
+  type === "ordered" ? (
+    <ol className="cdx-block cdx-list--ordered">{content}</ol>
+  ) : (
+    <ul className="cdx-block cdx-list--unordered">{content}</ul>
+  );
+};
+
+export default List;
