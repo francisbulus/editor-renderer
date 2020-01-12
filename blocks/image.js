4bf40c087cdf056e08370becee8e4bc56c1a2e15
Francis Bulus <notchera@gmail.com>
2020-01-12 15:09:41 +0100

    completed renderer for image

--- blocks/image.js
+++ blocks/image.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import ReactHtmlParser from 'react-html-parser';
-
+import React from "react";
+import ReactHtmlParser from "react-html-parser";
 
 const ImageOutput = data => {
-	if (!data || !data.file || !data.file.url) return '';
+  if (!data || !data.file || !data.file.url) return "";
 
-	const captionStyle = {
-        margin: '0 auto',
-        fontStyle: 'italic',
-	};
+  const captionStyle = {
+    margin: "0 auto",
+    fontStyle: "italic"
+  };
 
-	return (
-		<div className="cdx-block cdx-simple-image">
-            <div className="cdx-simple-image__picture">
-			<img src={ data.file.url } alt={ data.caption || '' } style={ imageStyle } />
-            </div>
-			{ data.caption && <figcaption style={ ...captionStyle }>{ ReactHtmlParser(data.caption) }</figcaption> }
-		</div>
-	);
+  return (
+    <div className="cdx-block cdx-simple-image">
+      <div className="cdx-simple-image__picture">
+        <img src={data.file.url} alt={data.caption || ""} style={imageStyle} />
+      </div>
+      {data.caption && (
+        <figcaption style={captionStyle}>
+          {ReactHtmlParser(data.caption)}
+        </figcaption>
+      )}
+    </div>
+  );
 };
 
 export default ImageOutput;
